1c233fbfff6e9513f38dd215197e1fe81e92c2c1
PapierKorb76 <irafrancesco894@gmail.com>
2022-05-19 21:11:41 +0200

    Fixedcounter

--- index.js
+++ index.js
@@ -7,26 +7,19 @@ const playerPoints = document.querySelector('#player-h2');
 const computerSign = document.querySelector('.computer-points');
 const playerSign = document.querySelector('.player-points');
 
+btnScissors.addEventListener('click', () => {
+    return playRound(btnScissors.id, computerPlay());
+});
 
-/*
-console.log(btnScissors.id);
-console.log(btnRock.id);
-console.log(btnPaper.id);
-*/
+btnRock.addEventListener('click', () => {
+    return playRound(btnScissors.id, computerPlay());
+});
 
-    btnScissors.addEventListener('click', () => {
-        return playRound(btnScissors.id, computerPlay());
-    });
-    
-    btnRock.addEventListener('click', () => {
-        return playRound(btnScissors.id, computerPlay());
-    });
-    
-    
-    btnPaper.addEventListener('click', () => {
-        return playRound(btnScissors.id, computerPlay());
-    });
-    
+
+btnPaper.addEventListener('click', () => {
+    return playRound(btnScissors.id, computerPlay());
+});
+
 
 
 function computerPlay() {
@@ -54,6 +47,8 @@ function playRound(playerSelection, computerSelection) {
     const content = document.createElement('div');
 
     playerSelection = playerSelection.toLowerCase();
+
+
     if (playerSelection == computerSelection) {
         content.classList.add('content');
         content.textContent = 'Nobody Won!';
@@ -61,46 +56,31 @@ function playRound(playerSelection, computerSelection) {
     } else if ((playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "rock")) {
-            playerCont ++;
-            playerPoints.innerHTML = playerCont;
-            content.classList.add('content');
-            content.textContent = "You won! The " + playerSelection + " beats the " + computerSelection + "!";
-            content.style.color = 'green';
-            resultsContainer.appendChild(content);
+        playerCont++;
+        playerPoints.innerHTML = playerCont;
+        content.classList.add('content');
+        content.textContent = "You won! The " + playerSelection + " beats the " + computerSelection + "!";
+        content.style.color = 'green';
+        resultsContainer.appendChild(content);
+    } else if (playerCont == 5) {
+        content.classList.add('content');
+        content.textContent = "Player win this match!";
+        content.style.color = 'yellow';
+        resultsContainer.appendChild(content);
+        document.location.reload();
+    } else if (PCCont == 5) {
+        content.classList.add('content');
+        content.textContent = "PC win this match!";
+        content.style.color = 'yellow';
+        resultsContainer.appendChild(content);
+        document.location.reload();
+
     } else {
-            PCCont ++;
-            computerPoints.innerHTML = PCCont;
-            content.classList.add('content');
-            content.textContent = "You lost! The " + computerSelection + " beats the " + playerSelection + "!";
-            content.style.color = 'red';
-            resultsContainer.appendChild(content);
+        PCCont++;
+        computerPoints.innerHTML = PCCont;
+        content.classList.add('content');
+        content.textContent = "You lost! The " + computerSelection + " beats the " + playerSelection + "!";
+        content.style.color = 'red';
+        resultsContainer.appendChild(content);
     }
 }
-
-
-
-// function game() {
-//     let playerCont = 0;
-//     let PCCont = 0;
-//     while(playerCont != 5 || PCCont != 5)
-//     computerSelection = computerPlay();
-//         if (content.style.color == 'green') {
-//             playerCont++;
-//             playerPoints.style.color = 'blue';
-//             playerPoints = playerCont;
-//         } else if (playRound(playerSelection, computerSelection).includes("You lost!")) {
-//             PCCont++;
-//         } else if ("Nobody won!") {
-//             playerCont == playerCont;
-//             PCCont == PCCont;
-//         }
-//         console.log("Player: " + playerCont + "   VS   " + "PC: " + PCCont);
-
-//     if (playerCont > PCCont) {
-//         return "You won the game with " + playerCont + " points";
-//     } else if (playerCont < PCCont) {
-//         return "You lost the game, the PC Won with " + PCCont + " points";
-//     } else {
-//         return "Tie! " + "Player Points: " + playerCont + " and " + "PC Points: " + PCCont;
-//     }
-// }
